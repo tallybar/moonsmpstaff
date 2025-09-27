e53650b4210e9a56853e4de2c069424717228ce3
tallybar <b.utkristaa@gmail.com>
2025-09-27 15:25:43 +0545

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js
+module.exports = {
+  correctPassword: "iamgay", // Change to your own secure password
+  webhookURLs: [
+    "https://discord.com/api/webhooks/1421382701734301726/Mf7VZtePkcAapEEbj16bxdJY5w_bX1-ALHreKTMKcxV3VhN14_hKI_8AxyITrAHB4PHj",
+    "https://discord.com/api/webhooks/SECOND_WEBHOOK_HERE"
+  ]
+};
